13ee7d92d690288902de2a816f7bba36d9932e7b
Juho Teperi <juho.teperi@iki.fi>
2016-05-25 20:34:28 +0300

    Moved modified eftest functions to separate ns, and small tuning

--- src/metosin/boot_alt_test/impl.clj
+++ src/metosin/boot_alt_test/impl.clj
@@ -3,56 +3,12 @@
             [clojure.tools.namespace.track :as track]
             [clojure.tools.namespace.reload :as reload]
             [clojure.string :as string]
-            [clojure.test :as test]
             [eftest.runner :as runner]
-            [eftest.report :as report]
-            [eftest.report.progress :as progress]
+            [metosin.boot-alt-test.eftest :as eftest]
             [boot.util :as util]
             [boot.pod :as pod]))
 
 (def tracker (atom nil))
-
-;; From eftest: https://github.com/weavejester/eftest/blob/master/src/eftest/runner.clj
-(defn- synchronize [f]
-  (let [lock (Object.)] (fn [x] (locking lock (f x)))))
-
-(defn- test-vars [ns vars opts]
-  (let [once-fixtures (-> ns meta ::test/once-fixtures test/join-fixtures)
-        each-fixtures (-> ns meta ::test/each-fixtures test/join-fixtures)
-        report        (synchronize test/report)
-        test-var      (fn [v] (binding [test/report report] (test/test-var v)))]
-    (once-fixtures
-     (fn []
-       (if (:multithread? opts true)
-         (dorun (pmap (bound-fn [v] (each-fixtures #(test-var v))) vars))
-         (doseq [v vars] (each-fixtures #(test-var v))))))))
-
-(defn- test-ns [ns vars opts]
-  (let [ns (the-ns ns)]
-    (binding [test/*report-counters* (ref test/*initial-report-counters*)]
-      (test/do-report {:type :begin-test-ns, :ns ns})
-      (test-vars ns vars opts)
-      (test/do-report {:type :end-test-ns, :ns ns})
-      @test/*report-counters*)))
-
-(defn- test-all [vars opts]
-  (->> (group-by (comp :ns meta) vars)
-       (map (fn [[ns vars]] (test-ns ns vars opts)))
-       (apply merge-with +)))
-
-;; Modified to return the result map
-(defn run-tests [vars opts]
-  (let [start-time (System/currentTimeMillis)]
-    (if (empty? vars)
-      (println "No tests found.")
-      (binding [report/*context* (atom {})
-                test/report      (:report opts progress/report)]
-        (test/do-report {:type :begin-test-run, :count (count vars)})
-        (let [counters (test-all vars opts)
-              duration (- (System/currentTimeMillis) start-time)
-              counters (assoc counters :type :summary, :duration duration)]
-          (test/do-report counters)
-          counters)))))
 
 (defn reload-and-test
   [tracker {:keys [test-matcher parallel? report-sym]
@@ -70,14 +26,13 @@
         (util/fail "Error reloading: %s\n" (name (::reload/error-ns @tracker)))
         (throw (::reload/error @tracker)))
       (catch java.io.FileNotFoundException e
-        (swap! tracker (fn [tracker]
-                         (util/info "Reseting tracker due to file not found exception, all namespaces will be reloaded next time.\n")
-                         (track/tracker)))
+        (util/info "Reseting tracker due to file not found exception, all namespaces will be reloaded next time.\n")
+        (reset! tracker (track/tracker))
         (throw e)))
 
     (util/info "Testing: %s\n" (string/join ", " tests))
 
-    (run-tests
+    (eftest/run-tests
       (runner/find-tests tests)
       (->> {:multithread? parallel?
             :report (when report-sym
@@ -89,8 +44,6 @@
 (defn run [opts]
   (swap! tracker (fn [tracker]
                    (util/dbug "Scan directories: %s\n" (pr-str (:directories pod/env)))
-                   (if tracker
-                     (dir/scan-dirs tracker (:directories pod/env))
-                     (dir/scan-dirs (track/tracker) (:directories pod/env)))))
+                   (dir/scan-dirs (or tracker (track/tracker)) (:directories pod/env))))
 
   (reload-and-test tracker opts))
