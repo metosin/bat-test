c87b25b5c42bb348afc850fe7ec31091910073bd
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-02-07 11:53:23 -0500

    add project.clj to cli-no-tests project

---
+++ test-projects/cli-no-tests/project.clj
@@ -0,0 +1,8 @@
+(def bat-test-version
+  (:out ((requiring-resolve 'clojure.java.shell/sh)
+         "clojure" "-T:build" "install"
+         :dir "../..")))
+(defproject metosin-test/cli-no-tests "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :plugins [[metosin/bat-test ~bat-test-version]
+            [lein-pprint "1.3.2"]])
